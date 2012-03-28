b1026498f8bfbc75848f6d72781a421e08420158
Thomas Dohmke <thomas@dohmke.de>
2012-03-28 12:29:29 +0200

    Removed library flag.

--- gen/net/hockeyapp/android/R.java
+++ gen/net/hockeyapp/android/R.java
@@ -11,6 +11,6 @@ public final class R {
     public static final class attr {
     }
     public static final class string {
-        public static int app_name=0x7f020000;
+        public static final int app_name=0x7f020000;
     }
 }
