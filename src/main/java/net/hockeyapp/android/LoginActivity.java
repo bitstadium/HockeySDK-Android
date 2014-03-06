cc3c7babfe686881f4d3744f6e0e3ae8f3cf9cde
Patrick Eschenbach <patrick.eschenbach@devheads.de>
2014-03-06 17:44:03 +0100

    HockeyApp Account Authentication

---
+++ src/main/java/net/hockeyapp/android/LoginActivity.java
@@ -0,0 +1,233 @@
+package net.hockeyapp.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import net.hockeyapp.android.tasks.LoginTask;
+import net.hockeyapp.android.views.LoginView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <h4>Description</h4>
+ *
+ * Activity to authenticate the user.
+ *
+ * <h4>License</h4>
+ *
+ * <pre>
+ * Copyright (c) 2011-2013 Bit Stadium GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * </pre>
+ *
+ * @author Patrick Eschenbach
+ **/
+public class LoginActivity extends Activity implements View.OnClickListener {
+  /**
+   * URL for HockeyApp API
+   */
+  private String url;
+
+  /**
+   * The APP secret.
+   */
+  private String secret;
+
+  /**
+   * The Login Mode.
+   */
+  private int mode;
+
+  /**
+   * The LoginTask.
+   */
+  private LoginTask loginTask;
+
+  /**
+   * The Handler for the LoginTask.
+   */
+  private Handler loginHandler;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(new LoginView(this));
+
+    Bundle extras = getIntent().getExtras();
+    if (extras != null) {
+      url = extras.getString("url");
+      secret = extras.getString("secret");
+      mode = extras.getInt("mode");
+    }
+
+    configureView();
+    initLoginHandler();
+
+    Object object = getLastNonConfigurationInstance();
+    if (object != null) {
+      loginTask = (LoginTask) object;
+      loginTask.attach(this, loginHandler);
+    }
+  }
+
+  private void configureView() {
+    if (mode == LoginManager.LOGIN_MODE_EMAIL_ONLY) {
+      EditText passwordInput = (EditText) findViewById(LoginView.PASSWORD_INPUT_ID);
+      passwordInput.setVisibility(View.INVISIBLE);
+    }
+
+    Button loginButton = (Button) findViewById(LoginView.LOGIN_BUTTON_ID);
+    loginButton.setOnClickListener(this);
+  }
+
+  private void initLoginHandler() {
+    loginHandler = new Handler() {
+      @Override
+      public void handleMessage(Message msg) {
+        Bundle bundle = msg.getData();
+        boolean success = bundle.getBoolean("success");
+
+        if (success) {
+          finish();
+        }
+        else {
+          Toast.makeText(LoginActivity.this, "Login failed. Check your credentials.", 2000).show();
+        }
+      }
+    };
+  }
+
+/*
+2. Für E-Mail + Password ist ein POST auf /api/2/apps/APP_ID/identity/authorize auszuführen. Wenn erfolgreich,
+kommt im JSON eine “auid” zurück (authorized user id).
+
+3. Für nur E-Mail ist ein POST auf /api/2/apps/APP_ID/identity/check auszuführen. Wenn erfolgreich, kommt im
+JSON eine “iuid” zurück (identifierd user id)
+
+4. Für die Prüfung, ob der User immer noch Mitglied ist, ist dann bei jedem Start oder nach einem Update ein
+GET /api/2/apps/APP_ID/identity/validate auszuführen, wobei entweder die auid oder iuid zu übergeben ist.
+Es kommt dann ein JSON mit Status zurück.
+*/
+
+  private void performAuthentication() {
+    String email = ((EditText) findViewById(LoginView.EMAIL_INPUT_ID)).getText().toString();
+    String password = ((EditText) findViewById(LoginView.PASSWORD_INPUT_ID)).getText().toString();
+
+    boolean ready = false;
+    Map<String, String> params = new HashMap<String, String>();
+
+    if (mode == LoginManager.LOGIN_MODE_EMAIL_ONLY) {
+      ready = !TextUtils.isEmpty(email);
+      params.put("email", email);
+      params.put("authcode", md5(secret + email));
+    }
+    else if (mode == LoginManager.LOGIN_MODE_EMAIL_PASSWORD) {
+      ready = !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password);
+      params.put("email", email);
+      params.put("password", password);
+    }
+
+    if (ready) {
+      loginTask = new LoginTask(this, loginHandler, url, mode, params);
+      loginTask.execute();
+    }
+    else {
+      Toast.makeText(this, "Please fill in the missing account credentials.", 1000).show();
+    }
+  }
+
+  public String md5(final String s) {
+    try {
+      // Create MD5 Hash
+      MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+      digest.update(s.getBytes());
+      byte messageDigest[] = digest.digest();
+
+      // Create Hex String
+      StringBuilder hexString = new StringBuilder();
+      for (byte aMessageDigest : messageDigest) {
+        String h = Integer.toHexString(0xFF & aMessageDigest);
+        while (h.length() < 2)
+          h = "0" + h;
+        hexString.append(h);
+      }
+      return hexString.toString();
+
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
+    return "";
+  }
+
+  @Override
+  public void onClick(View v) {
+    switch (v.getId()) {
+      case LoginView.LOGIN_BUTTON_ID:
+        performAuthentication();
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  @Override
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    if (keyCode == KeyEvent.KEYCODE_BACK) {
+      Intent intent = new Intent(this, LoginManager.mainActivity);
+      intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+      intent.putExtra(LoginManager.LOGIN_EXIT_KEY, true);
+      startActivity(intent);
+      return true;
+    }
+
+    return super.onKeyDown(keyCode, event);
+  }
+
+  /**
+   * Detaches the activity from the LoginTask and returns the task
+   * as last instance. This way the task is restored when the activity
+   * is immediately re-created.
+   *
+   * @return The login task if present and null otherwise.
+   */
+  @Override
+  public Object onRetainNonConfigurationInstance() {
+    if (loginTask != null) {
+      loginTask.detach();
+    }
+
+    return loginTask;
+  }
+}
---
+++ src/main/java/net/hockeyapp/android/LoginManager.java
@@ -0,0 +1,196 @@
+package net.hockeyapp.android;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import net.hockeyapp.android.tasks.LoginTask;
+import net.hockeyapp.android.utils.PrefsUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <h4>Description</h4>
+ *
+ * The LoginManager displays the auth activity.
+ *
+ * <h4>License</h4>
+ *
+ * <pre>
+ * Copyright (c) 2011-2013 Bit Stadium GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * </pre>
+ *
+ * @author Patrick Eschenbach
+ **/
+public class LoginManager {
+  public static final int LOGIN_MODE_ANONYMOUS      = 0;
+  public static final int LOGIN_MODE_EMAIL_ONLY     = 1;
+  public static final int LOGIN_MODE_EMAIL_PASSWORD = 2;
+  public static final int LOGIN_MODE_VALIDATE       = 3;
+
+  /**
+   * The key for the intent of the main activity.
+   */
+  static final String LOGIN_EXIT_KEY = "net.hockeyapp.android.EXIT";
+
+  /**
+   * The entry activity of this app.
+   */
+  static Class<?> mainActivity;
+
+
+  /**
+   * App identifier from HockeyApp.
+   */
+  private static String identifier = null;
+
+  /**
+   * App secret from HockeyApp.
+   */
+  private static String secret = null;
+
+  /**
+   * Handler for uid validation.
+   */
+  private static Handler validateHandler = null;
+
+  /**
+   * The Login Mode.
+   */
+  private static int mode;
+
+  /**
+   * Registers new LoginManager.
+   *
+   * @param context The context to use. Usually your Activity object.
+   * @param appIdentifier App ID of your app on HockeyApp.
+   * @param mode The Login Mode.
+   * @param activity The entry activity of the app.
+   */
+  public static void register(final Context context, String appIdentifier, String secret, int mode, Class<?> activity) {
+    if (context != null) {
+      LoginManager.identifier = appIdentifier;
+      LoginManager.secret = secret;
+      LoginManager.mode = mode;
+      LoginManager.mainActivity = activity;
+
+      LoginManager.validateHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+          Bundle bundle = msg.getData();
+          boolean success = bundle.getBoolean("success");
+
+          if (!success) {
+            startLoginActivity(context);
+          }
+        }
+      };
+
+      Constants.loadFromContext(context);
+    }
+  }
+
+  /**
+   * Starts the {@link LoginActivity}
+   *
+   * @param context {@link android.content.Context} object
+   */
+  public static void verifyLogin(Activity context, Intent intent) {
+    // Check if application needs to be exited.
+    if (intent != null) {
+      if (intent.getBooleanExtra(LOGIN_EXIT_KEY, false)) {
+        context.finish();
+        return;
+      }
+    }
+
+    if (context == null || mode == LOGIN_MODE_ANONYMOUS || mode == LOGIN_MODE_VALIDATE) {
+      return;
+    }
+
+    SharedPreferences prefs = context.getSharedPreferences("net.hockeyapp.android.login", 0);
+    int currentMode = prefs.getInt("mode", -1);
+    if (currentMode != mode) {
+      PrefsUtil.applyChanges(prefs.edit()
+          .remove("auid")
+          .remove("iuid")
+          .putInt("mode", mode));
+    }
+
+    String auid = prefs.getString("auid", null);
+    String iuid = prefs.getString("iuid", null);
+
+    boolean notAuthenticated = auid == null && iuid == null;
+    boolean auidMissing      = auid == null && mode == LOGIN_MODE_EMAIL_PASSWORD;
+    boolean iuidMissing      = iuid == null && mode == LOGIN_MODE_EMAIL_ONLY;
+
+    if (notAuthenticated || auidMissing || iuidMissing) {
+      startLoginActivity(context);
+      return;
+    }
+
+    Map<String, String> params = new HashMap<String, String>();
+    if (auid != null) {
+      params.put("type", "auid");
+      params.put("id", auid);
+    }
+    else if (iuid != null) {
+      params.put("type", "iuid");
+      params.put("id", iuid);
+    }
+
+    LoginTask verifyTask = new LoginTask(context, validateHandler, getURLString(LOGIN_MODE_VALIDATE), LOGIN_MODE_VALIDATE, params);
+    verifyTask.setShowProgressDialog(false);
+    verifyTask.execute();
+  }
+
+  private static void startLoginActivity(Context context) {
+    Intent intent = new Intent();
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_HISTORY);
+    intent.setClass(context, LoginActivity.class);
+    intent.putExtra("url", getURLString(mode));
+    intent.putExtra("mode", mode);
+    intent.putExtra("secret", secret);
+    context.startActivity(intent);
+  }
+
+  private static String getURLString(int mode) {
+    String suffix = "";
+    if (mode == LOGIN_MODE_EMAIL_PASSWORD) {
+      suffix = "authorize";
+    }
+    else if (mode == LOGIN_MODE_EMAIL_ONLY) {
+      suffix = "check";
+    }
+    else if (mode == LOGIN_MODE_VALIDATE) {
+      suffix = "validate";
+    }
+
+    return Constants.BASE_URL + "api/3/apps/" + identifier + "/identity/" + suffix;
+  }
+}
