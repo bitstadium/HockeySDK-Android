8caea8cb8eb2f3c8c7b9c01c2ec6c85a06661767
Matthias Wenz <mat@bowstreet.de>
2015-08-27 10:21:01 +0200

    New version

--- src/main/java/net/hockeyapp/android/Constants.java
+++ src/main/java/net/hockeyapp/android/Constants.java
@@ -111,7 +111,7 @@ public class Constants {
   /**
    * Version of this SDK.
    */
-  public static final String SDK_VERSION = "3.5.0";
+  public static final String SDK_VERSION = "3.6.0";
 
   /**
    * Initializes constants from the given context. The context is used to set 
