5cc26355140fd52ca593b1eeb7a089a4046047a2
Matthias Wenz <mat@bowstreet.de>
2016-01-11 15:08:43 +0100

    Add test suite for instrumentation tests
    Apparently this is necessary for connectedCheck tests to be run properly. Really?! ffs, Android...

---
+++ hockeysdk/src/androidTest/java/net/hockeyapp/android/suite/InstrumentationTestSuite.java
@@ -0,0 +1,12 @@
+package net.hockeyapp.android.suite;
+
+import net.hockeyapp.android.ExceptionHandlerTest;
+import net.hockeyapp.android.TrackingTest;
+import net.hockeyapp.android.UtilTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ExceptionHandlerTest.class, TrackingTest.class, UtilTest.class})
+public class InstrumentationTestSuite {
+}
