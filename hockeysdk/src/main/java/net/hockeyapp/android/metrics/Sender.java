63d359e21cedbb868038dfec10b1a4f2b5f5c97a
Benjamin Reimold <bereimol@microsoft.com>
2016-01-12 12:13:12 -0800

    Change default server URL to real endpoint

--- hockeysdk/src/main/java/net/hockeyapp/android/metrics/Sender.java
+++ hockeysdk/src/main/java/net/hockeyapp/android/metrics/Sender.java
@@ -28,42 +28,14 @@ import java.util.zip.GZIPOutputStream;
  * <p>
  * Either calls execute or executeOnExecutor on an AsyncTask depending on the
  * API level.
- * </p>
- * <h3>License</h3>
- *
- * <pre>
- * Copyright (c) 2011-2015 Bit Stadium GmbH
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- * </pre>
- *
- * @author Benjamin Reimold
- */
+**/
+
 public class Sender {
 
   /**
    * Default endpoint where all data will be send.
    */
-  static final String DEFAULT_ENDPOINT_URL = "https://dc.services.visualstudio.com/v2/track";
+  static final String DEFAULT_ENDPOINT_URL = "https://gate.hockeyapp.net/v2/track";
 
   static final int DEFAULT_SENDER_READ_TIMEOUT = 10 * 1000;
   static final int DEFAULT_SENDER_CONNECT_TIMEOUT = 15 * 1000;
