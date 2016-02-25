2d5f7d78721ecd12fe5a939afae69951c6658759
Matthias Wenz <mat@bowstreet.de>
2016-02-24 21:06:38 -0800

    Add doc header for logging wrapper

--- hockeysdk/src/main/java/net/hockeyapp/android/utils/HockeyLog.java
+++ hockeysdk/src/main/java/net/hockeyapp/android/utils/HockeyLog.java
@@ -4,6 +4,14 @@ package net.hockeyapp.android.utils;
 import android.util.Log;
 
 /**
+ * <h3>Description</h3>
+ *
+ * Wrapper class for logging in the SDK as well as
+ * setting the desired log level for end users.
+ * Log levels correspond to those of android.util.Log.
+ *
+ * @see android.util.Log
+ *
  * <h3>License</h3>
  * <p/>
  * <pre>
